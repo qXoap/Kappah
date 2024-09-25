4d27f64827eb411617d3e9702665ef6b90705a8f
qXoap <92841319+qXoap@users.noreply.github.com>
2024-09-25 06:04:39 -0500

    Update KappahMode.java

--- src/main/java/dev/xoapp/kappah/scheduler/KappahMode.java
+++ src/main/java/dev/xoapp/kappah/scheduler/KappahMode.java
@@ -7,43 +7,40 @@ import cn.nukkit.entity.effect.EffectType;
 import cn.nukkit.scheduler.Task;
 import cn.nukkit.utils.TextFormat;
 import dev.xoapp.kappah.session.Session;
+import dev.xoapp.kappah.session.SessionFactory;
 
 public class KappahMode extends Task {
 
-    private final Session _session;
-
-    public KappahMode(Session session) {
-        _session = session;
-    }
-
     @Override
     public void onRun(int i) {
-        Player player = _session.getPlayer();
+        for (Session _session : SessionFactory.getSessions().values()) {
+            Player player = _session.getPlayer();
 
-        if (player == null) {
-            getHandler().cancel();
-            return;
+            if (player == null) {
+                getHandler().cancel();
+                return;
+            }
+
+            if (_session.isNull()) {
+                getHandler().cancel();
+                return;
+            }
+
+            if (_session.isVanish()) {
+                player.addEffect(Effect.get(EffectType.INVISIBILITY).setVisible(false));
+                player.addEffect(Effect.get(EffectType.NIGHT_VISION).setVisible(false));
+            }
+
+            String isVanish = _session.isVanish() ? "&aEnabled" : "&cDisabled";
+
+            String tipMessage = String.format(
+                    "&fOnline: &e%s &7| &fTPS: &e%s &7| &fVanish: &r%s",
+                    Server.getInstance().getOnlinePlayers().size(),
+                    Server.getInstance().getTicksPerSecond(),
+                    isVanish
+            );
+
+            player.sendTip(TextFormat.colorize(tipMessage));
         }
-
-        if (_session.isNull()) {
-            getHandler().cancel();
-            return;
-        }
-
-        if (_session.isVanish()) {
-            player.addEffect(Effect.get(EffectType.INVISIBILITY).setVisible(false));
-            player.addEffect(Effect.get(EffectType.NIGHT_VISION).setVisible(false));
-        }
-
-        String isVanish = _session.isVanish() ? "&aEnabled" : "&cDisabled";
-
-        String tipMessage = String.format(
-                "&fOnline: &e%s &7| &fTPS: &e%s &7| &fVanish: &r%s",
-                Server.getInstance().getOnlinePlayers().size(),
-                Server.getInstance().getTicksPerSecond(),
-                isVanish
-        );
-
-        player.sendTip(TextFormat.colorize(tipMessage));
     }
 }
